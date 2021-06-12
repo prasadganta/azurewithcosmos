28a7b36213a8fde05c7eb8ca24863a3678a41ae9
prasad ganta <prasadganta@prasads-MacBook-Air.local>
2021-06-12 14:49:01 +0530

    first commit

---
+++ src/main/java/com/azure/tony/azurewithcosmos/AzurewithcosmosApplication.java
@@ -0,0 +1,13 @@
+package com.azure.tony.azurewithcosmos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzurewithcosmosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzurewithcosmosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/azure/tony/azurewithcosmos/AzurewithcosmosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azure.tony.azurewithcosmos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzurewithcosmosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
